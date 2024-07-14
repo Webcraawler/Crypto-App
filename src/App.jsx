f73b2b36dc284265896776f164729cdbebdc327a
Webcraawler <zeeshanakhtar2611@gmail.com>
2024-07-15 01:47:57 +0530

    Crypto App

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import { Home } from "./components/Home";
+import { Coins } from "./components/Coins";
+import { Exchange } from "./components/Exchange";
+import { CoinDetails } from "./components/CoinDetails";
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coins" element={<Coins />} />
+        <Route path="/exchange" element={<Exchange />} />
+        <Route path="/coin/:id" element={<CoinDetails />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/CoinDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CoinDetails = () => {
+  return (
+    <div>CoinDetails</div>
+  )
+}
